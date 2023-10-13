0a7337e47aefa88dc02c9f8a89e528bc1a475da9
Janaka Daskon <janakadaskon@gmail.com>
2023-10-13 23:23:55 +0530

    load the stage

---
+++ POS/smart_pos/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,28 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/MainForm.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Home");
+        primaryStage.setMaximized(true);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+    }
+}
---
+++ POS/smart_pos/src/main/java/lk/ijse/dep11/AppInitializerWrapper.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep11;
+
+public class AppInitializerWrapper {
+
+    public static void main(String[] args) {
+
+        AppInitializer.main(args);
+    }
+
+
+}
