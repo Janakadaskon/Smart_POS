2dba6a4866b96b32280a3e1aadfcdea6a3399114
Janaka Daskon <janakadaskon@gmail.com>
2023-10-28 04:35:30 +0530

    Update the README.md

--- POS/smart_pos/src/main/java/lk/ijse/dep11/controller/ManageCustomerFormController.java
+++ POS/smart_pos/src/main/java/lk/ijse/dep11/controller/ManageCustomerFormController.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 public class ManageCustomerFormController {
 
@@ -17,6 +21,11 @@ public class ManageCustomerFormController {
     public Button btnDelete;
     public TableView tblCusomer;
 
+
+    public void navigateToHome(javafx.scene.input.MouseEvent mouseEvent)throws IOException{
+        MainFormController.navigateToMain(root);
+    }
+
     public void btnNewCustomerOnAction(ActionEvent event) {
     }
 
@@ -25,4 +34,6 @@ public class ManageCustomerFormController {
 
     public void btnDeleteOnAction(ActionEvent event) {
     }
+
+
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Start the project
 UI design of home page(Done up to now)
 UI design of Manage Customers has done up to now
+Done navigation between home and customer management window
