fbf0cc7bff0212b36d4ff508965cf66f879250ba
Janaka Daskon <janakadaskon@gmail.com>
2023-11-19 05:39:20 +0530

    create the neccessary tables in the database

---
+++ POS/smart_pos/src/main/java/lk/ijse/dep11/controller/PlaceOrderFormController.java
@@ -0,0 +1,32 @@
+package lk.ijse.dep11.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class PlaceOrderFormController {
+    public ImageView imgHome;
+    public Label lblOrderId;
+    public Label lblDate;
+    public TextField txtName;
+    public TextField txtDescription;
+    public TextField txtQtyInHand;
+    public TextField txtPrice;
+    public TextField txtQty;
+    public Button btnAdd;
+    public TableView tblOrder;
+    public Label txtTotal;
+    public Button btnOrder;
+
+    public void btnAddOnAction(ActionEvent event) {
+    }
+
+    public void btnOrderOnAction(ActionEvent event) {
+    }
+
+
+
+}
